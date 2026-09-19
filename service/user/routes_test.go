76a8bd55c708690ee2fc4ecf816ae550404652d8
Guiziin227 <gh63388@gmail.com>
2026-09-19 19:46:38 -0300

    adiciona testes para o manipulador de registro de usuário

---
+++ service/user/routes_test.go
@@ -0,0 +1,60 @@
+package user
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/guiziin227/goApiJwt/types"
+)
+
+func TestUserServiceHandlers(t *testing.T) {
+
+	userStore := &mockUserStore{}
+
+	handler := NewHandler(userStore)
+
+	t.Run("Should fail if the payload is invalid", func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "Guilherme",
+			LastName:  "Henriques",
+			Email:     "as",
+			Password:  "password",
+		}
+
+		marshalled, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+
+		router.ServeHTTP(rr, req)
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("Expected status code %d, but got %d", http.StatusBadRequest, rr.Code)
+		}
+	})
+}
+
+type mockUserStore struct{}
+
+func (m *mockUserStore) GetUserByEmail(email string) (*types.User, error) {
+	return nil, fmt.Errorf("user not found")
+}
+
+func (m *mockUserStore) GetUserById(id int) (*types.User, error) {
+	return nil, nil
+}
+
+func (m *mockUserStore) CreateUser(user *types.User) error {
+	return nil
+}
