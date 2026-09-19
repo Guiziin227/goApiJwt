a3f536cb5edb11432eca79fa37df60d119ea9231
Guiziin227 <gh63388@gmail.com>
2026-09-19 11:11:30 -0300

    main de inicialização

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/guiziin227/goApiJwt/cmd/api"
+)
+
+func main() {
+	server := api.NewAPIServer(":8080", nil)
+
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
