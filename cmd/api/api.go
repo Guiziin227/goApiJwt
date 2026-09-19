1f3cc7bf53d3b088b2712c0b4880391d5f179be0
Guiziin227 <gh63388@gmail.com>
2026-09-19 11:43:40 -0300

    adicionando explicacoes

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -21,10 +21,18 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 	}
 }
 
+// Run monta o servidor HTTP e inicia a escuta na porta especificada.
+// Ele cria um roteador principal usando o pacote Gorilla Mux, define um subroteador para a versão da API ("/api/v1") e
+// registra os manipuladores de rotas do usuário.
+// Por fim, ele inicia o servidor HTTP e retorna qualquer erro que ocorra durante a execução.
 func (s *APIServer) Run() error {
 
+	// O roteador principal é criado usando o pacote Gorilla Mux, q
+	//ue fornece funcionalidades avançadas de roteamento para aplicativos web em Go.
 	router := mux.NewRouter()
 
+	// O subroteador é criado para agrupar as rotas da versão da API ("/api/v1").
+	// Isso permite que todas as rotas relacionadas à API sejam organizadas sob um prefixo comum.
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
 	userHandler := user.NewHandler()
